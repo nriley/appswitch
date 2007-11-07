fa0b7f6659cd787ef0ad8846b0814741ef7196f3
Nicholas Riley <appswitch@sabi.net>
2007-11-07 03:55:29 +0000

    svn:ignore: added appswitch, now it builds in the top-level directory.
    README: Fixed installation instructions, now appswitch builds in the
    top-level directory.
    
    appswitch.xcodeproj: Add Xcode 3 specific stuff; remove detritus from
    install build.
    
    package-appswitch.sh: Update based on package-launch.sh from [305] and
    [312]: simplifies and moves executable out of build directory;
    properly creates man directory and sets permissions on man page;
    removes mentions of .dmg packaging; excludes more useless things
    instead of ineffectively deleting them.

--- package-appswitch.sh
+++ package-appswitch.sh
@@ -4,33 +4,18 @@ set -x -v
 
 cd appswitch && \
 find . -name \*~ -exec rm '{}' \; && \
-xcodebuild && \
-SetFile -c 'R*ch' -t 'TEXT' README VERSION && \
-strip build/appswitch && \
-sudo /usr/bin/install -c build/appswitch /usr/local/bin && \
-sudo /usr/bin/install -c appswitch.1 /usr/local/man/man1 && \
-chmod 755 build/appswitch && \
+xcodebuild -configuration Deployment clean && \
+xcodebuild -configuration Deployment DSTROOT=/ "INSTALL_PATH=$PWD" install && \
+SetFile -c 'ttxt' -t 'TEXT' README VERSION appswitch.1 && \
+sudo /usr/bin/install -d /usr/local/bin /usr/local/man/man1 && \
+sudo /usr/bin/install appswitch /usr/local/bin && \
+sudo /usr/bin/install -m 644 appswitch.1 /usr/local/man/man1 && \
+chmod 755 appswitch && \
 chmod 644 appswitch.1 && \
-rm -rf build/appswitch.build build/intermediates build/.gdb_history && \
 VERSION=`cat VERSION` TARBALL="appswitch-$VERSION.tar.gz" && \
-DMG="appswitch-$VERSION.dmg" VOL="appswitch $VERSION" MOUNTPOINT="/Volumes/$VOL" && \
 cd .. && \
-rm -f appswitch-$VERSION $TARBALL $DMG && \
+rm -f appswitch-$VERSION $TARBALL && \
 ln -s appswitch appswitch-$VERSION && \
-tar --owner=root --group=wheel --exclude=.DS_Store --exclude=.svn --exclude=.gdb_history -zchf appswitch-$VERSION.tar.gz appswitch-$VERSION && \
-#hdiutil create $DMG -megabytes 5 -ov -type UDIF && \
-#DISK=`hdid $DMG | sed -ne ' /Apple_partition_scheme/ s|^/dev/\([^ ]*\).*$|\1|p'` && \
-#newfs_hfs -v "$VOL" /dev/r${DISK}s2 && \
-#hdiutil eject $DISK && \
-#hdid $DMG && \
-#ditto -rsrc launch "$MOUNTPOINT" && \
-#ditto -rsrc "InstallAnywhere/launch_Web_Installers/InstData/MacOSX/Install launch $VERSION.sit" "/Volumes/launch $VERSION" && \
-#launch "$MOUNTPOINT/Install launch $VERSION.sit" && \
-#./openUp "$MOUNTPOINT" && \
-#sleep 2 && \
-## hdiutil eject $DISK && \ # this doesn't work
-#osascript -e "tell application \"Finder\" to eject disk \"$VOL\"" && \
-#hdiutil convert $DMG -format UDZO -o z$DMG && \
-#mv z$DMG $DMG && \
-scp $TARBALL ainaz:web/nriley/software/ #$DMG 
+tar --owner=root --group=wheel --exclude=.DS_Store --exclude=.svn --exclude=.gdb_history --exclude=build --exclude=\*.mode* --exclude=\*.pbxuser --exclude=\*.perspective -zchf appswitch-$VERSION.tar.gz appswitch-$VERSION && \
+# scp $TARBALL ainaz:web/nriley/software/
 :
