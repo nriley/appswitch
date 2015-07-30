7e286df4b45e58b085b34ceea86f863e2b83a977
Nicholas Riley <com-github@sabi.net>
2015-07-29 20:04:41 -0400

    Update copyright notice some more.

--- appswitch/main.m
+++ appswitch/main.m
@@ -2,7 +2,7 @@
  appswitch - a command-line application switcher
  Nicholas Riley <appswitch@sabi.net>
 
- Copyright (c) 2003-12, Nicholas Riley
+ Copyright (c) 2003-15, Nicholas Riley
  All rights reserved.
 
  Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
